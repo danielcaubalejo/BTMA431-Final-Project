75e3ab068e6be5e0f08466f685bda263afb57e69
AaronFDiwa <aaron4diwa@gmail.com>
2023-11-16 14:15:46 -0700

    Sectioned out the script with comments (Main questions, sub questions...)

--- FinalProject.R
+++ FinalProject.R
@@ -1,6 +1,39 @@
+# Installing and loading packages
 install.packages("rvest")
-install.packages("dplyr")
-# test out changes
-
 library(rvest)
+install.packages("dplyr")
 library(dplyr)
+
+#### Main Question 1: How are cinemas being affected by streaming services? ####
+
+## How are theaters performing since the rise of streaming services? ##
+
+
+
+## How long after a movie release does it take for it to air on streaming platforms?
+
+
+
+
+#### Main Question 2: How does movie industry performance compare before and after the pandemic? ####
+
+
+## How do streaming service change TV industries?
+## (What is the impact on Cable TV, news, Daily Shows, etc.)
+
+
+
+## How has the number of people that use cable TV changed over time? How has broadcasting network revenue changed?
+
+
+
+
+
+#### Main Question 3: How has taste, in terms of movies sentiment changed over time? ####
+
+## What genre has the most frequency of social media buzz?
+## (Based on # of views, # of comments, Top search, frequency of words)
+## (What words appear most often?)
+
+
+## What is the most popular genre per year?
