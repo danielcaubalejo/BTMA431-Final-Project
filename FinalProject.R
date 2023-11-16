964a42eaec4cfe11aed91456adaebc552458356e
AaronFDiwa <aaron4diwa@gmail.com>
2023-11-16 14:17:26 -0700

    Making question seperation more clear with comments

--- FinalProject.R
+++ FinalProject.R
@@ -4,6 +4,8 @@ library(rvest)
 install.packages("dplyr")
 library(dplyr)
 
+####################################################################################################################
+
 #### Main Question 1: How are cinemas being affected by streaming services? ####
 
 ## How are theaters performing since the rise of streaming services? ##
@@ -13,6 +15,8 @@ library(dplyr)
 ## How long after a movie release does it take for it to air on streaming platforms?
 
 
+
+####################################################################################################################
 
 
 #### Main Question 2: How does movie industry performance compare before and after the pandemic? ####
@@ -27,7 +31,7 @@ library(dplyr)
 
 
 
-
+####################################################################################################################
 
 #### Main Question 3: How has taste, in terms of movies sentiment changed over time? ####
 
@@ -37,3 +41,4 @@ library(dplyr)
 
 
 ## What is the most popular genre per year?
+
