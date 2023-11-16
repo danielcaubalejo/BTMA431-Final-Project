4674a3b92f33eb92bc848c5286e6e2202576967c
AaronFDiwa <aaron4diwa@gmail.com>
2023-11-16 14:31:22 -0700

    Added annual.ticket.sales data from https://www.the-numbers.com/market/

--- FinalProject.R
+++ FinalProject.R
@@ -22,12 +22,29 @@ library(dplyr)
 #### Main Question 2: How does movie industry performance compare before and after the pandemic? ####
 
 
-## How do streaming service change TV industries?
+## How do streaming services change TV industries?
 ## (What is the impact on Cable TV, news, Daily Shows, etc.)
 
 
 
-## How has the number of people that use cable TV changed over time? How has broadcasting network revenue changed?
+## How has the number of people that watch in cinemas change over time? How has box office revenue changed?
+
+# Sources:
+# https://www.statista.com/statistics/187073/tickets-sold-at-the-north-american-box-office-since-1980/
+# https://www.the-numbers.com/market/
+
+link = "https://www.the-numbers.com/market/"
+page = read_html(link)
+
+# Scraping Annual Ticket sales table
+year = page %>% html_nodes("center:nth-child(9) a") %>% html_text()
+tickets.sold = page %>% html_nodes("center:nth-child(9) .data:nth-child(2)") %>% html_text()
+total.box.office = page %>% html_nodes("center:nth-child(9) .data:nth-child(3)") %>% html_text()
+total.inflation.adj.box.office = page %>% html_nodes("center:nth-child(9) .data:nth-child(4)") %>% html_text()
+avg.ticket.price = page %>% html_nodes("center:nth-child(9) .data:nth-child(5)") %>% html_text()
+
+# Creating the scraped data into a data frame
+annual.ticket.sales.df <- data.frame(year, tickets.sold, total.box.office, total.inflation.adj.box.office, avg.ticket.price)
 
 
 
