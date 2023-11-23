2bad7d1f193bc45295a19e996b64553a5e8530ea
danielcaubalejo <danielcaubalejo@gmail.com>
2023-11-22 21:24:22 -0700

    solved row mismatch

--- movie_sentiment.R
+++ movie_sentiment.R
@@ -9,29 +9,32 @@ rD <- rsDriver(browser = "firefox",
 
 remDr <- rD[["client"]]
 
-scroll_navigation <- function(){
-  
-  #click load more code taken from: https://github.com/ggSamoora/TutorialsBySamoora/blob/main/rate_my_professor_script.Rmd
-  loadmore <- remDr$findElement(using = "xpath", value = '//*[@id="load-more-trigger"]')
-  
-  #initialize page number
-  page <- 0
-  #large enough page # until enough reviews needed
-  while (page <= 100) {
-    #Navigate to the webpage with the current page number
-    #url <- paste0("https://www.rottentomatoes.com/browse/movies_at_home/sort:popular?page=", page)
+scroll_navigation <- function() {
+  while(TRUE) {
+    # Check if the "Load More" button exists
+    loadmoreElem <- tryCatch({
+      remDr$findElements(using = "xpath", value = '//*[@id="load-more-trigger"]')
+    }, error = function(e) NULL)
     
-    #webpage scroll test
-    #y_position <- loadmore$getElementLocation()$y - 100
-    #remDr$executeScript(sprintf("window.scrollTo(0, %f)", y_position))
+    # Check if the "Load More" button's parent div has the 'ipl-load-more--loaded-all' class
+    allLoadedElem <- tryCatch({
+      remDr$findElements(using = "css", value = '.ipl-load-more.ipl-load-more--loaded-all')
+    }, error = function(e) NULL)
     
+    # If the "Load More" button does not exist or all reviews have been loaded, break the loop
+    if (length(loadmoreElem) == 0 || length(allLoadedElem) > 0) {
+      break
+    }
+    
+    # If the "Load More" button exists, click it
+    loadmore <- loadmoreElem[[1]]
     loadmore$clickElement()
-    # Increment the page number
-    page <- page + 1
+    
     # Pause code to ensure webpage loads properly
-    Sys.sleep(1.5)
+    #Sys.sleep(1.5)
   }
 }
+
 
 dropdown_all <- function() {
   # Assuming remDr and button_class are already defined
@@ -72,8 +75,21 @@ reviewElem1 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 't
 reviews1 <- unlist(lapply(reviewElem1, function(x){x$getElementText()}))
 reviews1 <- head(reviews1,1500)
 
-ratingElem1 <- remDr$findElements(using = "css", value = "span.rating-other-user-rating")
-ratings1 <- sapply(ratingElem1, function(x) {x$getElementText()}) %>% str_extract("\\d+")
+#due to some reviews having no ratings 
+all_reviews <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 'lister-item-content')]")
+
+ratings1 <- vector("character", length(all_reviews))
+
+for (i in seq_along(all_reviews)) {
+  review <- all_reviews[[i]]
+  
+  ratingElem <- tryCatch({
+    review$findChildElement("css", value = "span.rating-other-user-rating")
+  }, error = function(e) NULL)
+  
+  ratings1[i] <- if (!is.null(ratingElem)) ratingElem$getElementText() %>% str_extract("\\d+") else NA
+}
+
 ratings1 <- head(ratings1,1500)
 
 Whiplash_Reviews <- data.frame(usernames1, titles1, dates1, reviews1, ratings1)
@@ -106,8 +122,20 @@ reviewElem2 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 't
 reviews2 <- unlist(lapply(reviewElem2, function(x){x$getElementText()}))
 reviews2 <- head(reviews2,1500)
 
-ratingElem2 <- remDr$findElements(using = "css", value = "span.rating-other-user-rating")
-ratings2 <- sapply(ratingElem2, function(x) {x$getElementText()}) %>% str_extract("\\d+")
+all_reviews2 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 'lister-item-content')]")
+
+ratings2 <- vector("character", length(all_reviews2))
+
+for (i in seq_along(all_reviews2)) {
+  review <- all_reviews2[[i]]
+  
+  ratingElem <- tryCatch({
+    review$findChildElement("css", value = "span.rating-other-user-rating")
+  }, error = function(e) NULL)
+  
+  ratings2[i] <- if (!is.null(ratingElem)) ratingElem$getElementText() %>% str_extract("\\d+") else NA
+}
+
 ratings2 <- head(ratings2,1500)
 
 InfWar_Reviews <- data.frame(usernames2, titles2, dates2, reviews2, ratings2)
@@ -140,8 +168,20 @@ reviewElem3 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 't
 reviews3 <- unlist(lapply(reviewElem3, function(x){x$getElementText()}))
 reviews3 <- head(reviews3,1500)
 
-ratingElem3 <- remDr$findElements(using = "css", value = "span.rating-other-user-rating")
-ratings3 <- sapply(ratingElem3, function(x) {x$getElementText()}) %>% str_extract("\\d+")
+all_reviews3 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 'lister-item-content')]")
+
+ratings3 <- vector("character", length(all_reviews3))
+
+for (i in seq_along(all_reviews3)) {
+  review <- all_reviews3[[i]]
+  
+  ratingElem <- tryCatch({
+    review$findChildElement("css", value = "span.rating-other-user-rating")
+  }, error = function(e) NULL)
+  
+  ratings3[i] <- if (!is.null(ratingElem)) ratingElem$getElementText() %>% str_extract("\\d+") else NA
+}
+
 ratings3 <- head(ratings3,1500)
 
 Matrix_Reviews <- data.frame(usernames3, titles3, dates3, reviews3, ratings3)
@@ -174,8 +214,20 @@ reviewElem4 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 't
 reviews4 <- unlist(lapply(reviewElem4, function(x){x$getElementText()}))
 reviews4 <- head(reviews4,1500)
 
-ratingElem4 <- remDr$findElements(using = "css", value = "span.rating-other-user-rating")
-ratings4 <- sapply(ratingElem4, function(x) {x$getElementText()}) %>% str_extract("\\d+")
+all_reviews4 <- remDr$findElements(using = 'xpath', "//div[starts-with(@class, 'lister-item-content')]")
+
+ratings4 <- vector("character", length(all_reviews4))
+
+for (i in seq_along(all_reviews4)) {
+  review <- all_reviews4[[i]]
+  
+  ratingElem <- tryCatch({
+    review$findChildElement("css", value = "span.rating-other-user-rating")
+  }, error = function(e) NULL)
+  
+  ratings4[i] <- if (!is.null(ratingElem)) ratingElem$getElementText() %>% str_extract("\\d+") else NA
+}
+
 ratings4 <- head(ratings4,1500)
 
 Spirited_Reviews <- data.frame(usernames4, titles4, dates4, reviews4, ratings4)
@@ -187,8 +239,6 @@ write.csv(Spirited_Reviews, csv_file_path4, row.names = FALSE)
 # rD[["server"]]$stop()
 # system("taskkill /im java.exe /f") # Stops the java.exe
 # rm(list = ls())
-
-
 
 
 
